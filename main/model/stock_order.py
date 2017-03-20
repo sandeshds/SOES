695e767f5f3815ff99a8e5a9347bdd9b44158c4e
sandeshd <sandeshd@insandeshd.local>
2017-03-20 17:54:28 +0530

    handling error scenarios

--- main/model/stock_order.py
+++ main/model/stock_order.py
@@ -3,7 +3,7 @@ from constants import constants
 
 class StockOrder:
     def __init__(self, stock_id, side, company, quantity):
-        self.quantity = int(quantity)
+        self.quantity = quantity
         self.quantity_requested = quantity
         self.company = company
         self.side = side
@@ -14,7 +14,12 @@ class StockOrder:
 def create_stock_orders_from_list(stock_list):
     stock_orders = []
     for stock in stock_list:
-        new_stock_order = StockOrder(stock[0], stock[1], stock[2], stock[3])
+        try:
+            int(stock[3])
+        except ValueError:
+            print('invalid data , skipping stock with id : ', stock[0])
+            continue
+        new_stock_order = StockOrder(stock[0], stock[1], stock[2], int(stock[3]))
         stock_orders.append(new_stock_order)
     return stock_orders
 
--- test/test_create_stock_orders_from_list.py
+++ test/test_create_stock_orders_from_list.py
@@ -7,4 +7,9 @@ class TestCreateStockOrdersFromList(TestCase):
     def test_create_stock_orders_from_list(self):
         stock_list = [[1221, 'sell', 'abc', 1]]
         stock_orders = create_stock_orders_from_list(stock_list)
-        self.assertTrue(len(stock_orders) > 0)
+        self.assertTrue(len(stock_orders) == 1)
+
+    def test_should_skip_order_if_invalid_quantity(self):
+        stock_list = [[1221, 'sell', 'abc', 1], [1222, 'sell', 'abc', '1as']]
+        stock_orders = create_stock_orders_from_list(stock_list)
+        self.assertTrue(len(stock_orders) == 1)
